e8b4870febb895fb09b788c02050bc9234b76808
th3m4nd4r1n <mdua_be23@thapar.edu>
2024-07-07 15:48:16 +0530

    added orbitcontrols and wiremesh to icosahedral geo

--- main.js
+++ main.js
@@ -1,44 +1,46 @@
+
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
-console.log(THREE);
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";   //corrected the path here
 
-//canvas
-const canvas = document.querySelector("canvas.webgl");
-
-// Scene
+const w = window.innerWidth;
+const h = window.innerHeight;
+const renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setSize(w, h);
+document.body.appendChild(renderer.domElement);
+const fov = 75;
+const aspect_ratio = w/h;
+const near = 0.1;
+const far = 10;
+const camera = new THREE.PerspectiveCamera(fov, aspect_ratio, near, far);
+camera.position.z=2;
 const scene = new THREE.Scene();
+const geometry = new THREE.IcosahedronGeometry(1.0,2);
+const mat= new THREE.MeshStandardMaterial({
+    color: 0xffffff,
+    flatShading: true
+});
 
-// size
-const size = {
-   width: window.innerWidth,
-   height: window.innerHeight
-}
-
-// object
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshStandardMaterial({colour: 0xff00f0});
-const mesh = new THREE.Mesh(geometry, material);
+const hemilight = new THREE.HemisphereLight(0x0099ff, 0xaa5500);
+scene.add(hemilight);
+const mesh = new THREE.Mesh(geometry, mat);
 scene.add(mesh);
 
-//light
-const light = new THREE.HemisphereLight(0xff0000, 0xff000, 1);
-scene.add(light);
-
-// Camera
-const camera = new THREE.PerspectiveCamera(75, size.width / size.height);
-camera.position.z = 3;
-camera.position.y = 0.8;
-// camera.position.x = 2;
-scene.add(camera);
-
-
-//renderer
-const renderer = new THREE.WebGLRenderer({
-   canvas: canvas
+const wireMat = new THREE.MeshBasicMaterial({
+    color: 0xffffff,
+    wireframe: true
 });
-renderer.setSize(size.width, size.height);
-renderer.render(scene, camera);
 
-//orbit controls
-// const controls = new OrbitControls(camera, renderer.domElement);
-// scene.add(controls);
+const wireMesh = new THREE.Mesh(geometry, wireMat);
+mesh.add(wireMesh);
+wireMesh.scale.setScalar(1.001);
+mesh.scale.setScalar(1.1);
+
+function animate(){
+    requestAnimationFrame(animate);
+    //   mesh.scale.setScalar(Math.cos(t*0.001 +1));   //t not defined here so this won't work
+    //   mesh.rotation.y = t*0.001;
+    renderer.render(scene, camera);
+}
+animate();
+renderer.render(scene, camera);
+const controls = new OrbitControls(camera, renderer.domElement);  //this had scope issue, define it after camera rendered have been defined
